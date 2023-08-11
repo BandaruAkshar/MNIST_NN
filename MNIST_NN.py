e2a14d13b1a4dbb1bac23d501cd6b5afd0560dcc
Bandaru Akshar <129412748+BandaruAkshar@users.noreply.github.com>
2023-08-11 19:10:35 +0530

    Update MNIST_NN.py

--- MNIST_NN.py
+++ MNIST_NN.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-data = pd.read_csv('mnist_train.csv')
+data = pd.read_csv('mnist_test.csv')
 data = np.array(data)
 m, n = data.shape
 np.random.shuffle(data)
